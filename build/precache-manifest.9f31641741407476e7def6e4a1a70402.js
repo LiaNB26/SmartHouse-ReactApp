4122cd23a4bb6295d87c7e42560773826a3177b6
LiaNB26 <liabrachio@gmail.com>
2020-05-09 11:19:08 +0300

    uploading project

---
+++ build/precache-manifest.9f31641741407476e7def6e4a1a70402.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f856ba4a1dced3b0f4fa6aa101cf99cc",
+    "url": "/SmartHouse-ReactApp/index.html"
+  },
+  {
+    "revision": "eaac5de44d9ba42d0f42",
+    "url": "/SmartHouse-ReactApp/static/css/main.edfac55e.chunk.css"
+  },
+  {
+    "revision": "fe82ca01e8f4e7539aa9",
+    "url": "/SmartHouse-ReactApp/static/js/2.b6ce52fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SmartHouse-ReactApp/static/js/2.b6ce52fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaac5de44d9ba42d0f42",
+    "url": "/SmartHouse-ReactApp/static/js/main.477af082.chunk.js"
+  },
+  {
+    "revision": "8c3273c4f64005ff784f",
+    "url": "/SmartHouse-ReactApp/static/js/runtime-main.dc6630e7.js"
+  },
+  {
+    "revision": "31451d70b397133c99de1d8e74844287",
+    "url": "/SmartHouse-ReactApp/static/media/bathroom.31451d70.jpg"
+  },
+  {
+    "revision": "d7a2c5b939a693f63f92ce78d52d1582",
+    "url": "/SmartHouse-ReactApp/static/media/bedroom.d7a2c5b9.jpeg"
+  },
+  {
+    "revision": "240ceb6d07438c5d783de82eecf00220",
+    "url": "/SmartHouse-ReactApp/static/media/house.240ceb6d.jpg"
+  },
+  {
+    "revision": "32ce02f21856d9a73bdd7872cc1cfda8",
+    "url": "/SmartHouse-ReactApp/static/media/kitchen.32ce02f2.jpg"
+  },
+  {
+    "revision": "14d8366e3a901b925e882c120087f96e",
+    "url": "/SmartHouse-ReactApp/static/media/living.14d8366e.jpg"
+  }
+]);
